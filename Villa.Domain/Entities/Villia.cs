11691a4757bd54b86992c23e45430ac6ffd7f681
rameshthapaais <rath842@ess.ais.ac.nz>
2025-12-13 22:11:59 +1300

    Build the Class Library for the Clean Architecture

---
+++ Villa.Domain/Entities/Villia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villa.Domain.Entities
+{
+    public class Villia
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string? Description { get; set; }
+        public decimal PricePerNight { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public bool HasSwimmingPool { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime?UpdatedAt { get; set; }
+    }
+}
