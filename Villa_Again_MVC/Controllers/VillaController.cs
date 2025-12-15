2ea322e97a49e8b92c19b69f5bd0b1ee4d5b7501
rameshthapaais <rath842@ess.ais.ac.nz>
2025-12-15 23:48:42 +1300

    Create the controller and View for the Villa

---
+++ Villa_Again_MVC/Controllers/VillaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa.Infrastructure.Data;
+
+namespace Villa_Again_MVC.Controllers
+{
+
+    public class VillaController : Controller
+    {
+    private readonly ApplicationDbContext _context;
+
+    public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+           return View();
+        }
+    }
+}
