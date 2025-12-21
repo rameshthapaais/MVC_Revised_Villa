74164bdd12fed4bfef6fa69f3962abdff99852e8
rameshthapaais <rath842@ess.ais.ac.nz>
2025-12-21 22:04:09 +1300

    Added the Interfce IVillaRepository in the Application

---
+++ Villa.Application/Common/Interface/IVillaRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Domain.Entities;
+
+namespace Villa.Application.Common.Interface
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villia> GetAll(Expression<Func<Villia, bool>>? filter = null,string?includeProperties =null);
+        IEnumerable<Villia> Get(Expression<Func<Villia, bool>> filter,string? includeProperties = null);
+
+        void Add(Villia entity);
+        void Update(Villia entity);
+
+        void Remove(Villia entity);
+
+        void Save();
+
+    }
+}
